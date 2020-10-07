b560426f86f1d549d70377861a7091b8d5cca9ea
asamoah@goldmail.etsu.edu <nanajr-dotcom>
2020-10-07 17:11:07 +0000

    commit 10

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 import Location from './Location';
+import Weatherbox from './Weatherbox';
 
 
 function App() {
@@ -16,13 +17,7 @@ function App() {
           />
         </div>
          <Location/> 
-        <div className="weather-box">
-          <div className="temp">27&deg; </div>
-          <div className="icon">
-            <i className="wi wi-day-sunny"></i>
-          </div>
-          <div className="weather">Sunny</div>
-        </div>
+         <Weatherbox/> 
       </main>
     </div>
   );
