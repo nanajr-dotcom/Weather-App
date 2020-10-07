fda39fd21735a16dd9345ed80e95c27b8ad5cc4c
asamoah@goldmail.etsu.edu <nanajr-dotcom>
2020-10-07 19:11:16 +0000

    commit 10

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 function Searchbox() {
-    return (
-      <div className="search-box">
-        <input 
-        type="text" 
-        className="search-bar" 
-        placeholder="Search......" />
-      </div>
+    return ( 
+          <div class="search-box">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="City"
+              autoComplete="off"
+            />
+          </div>
+ 
     );
 }
 
