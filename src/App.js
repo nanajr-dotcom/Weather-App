7267411d05d19fe93d04e8da18b8deb957d00a14
asamoah@goldmail.etsu.edu <nanajr-dotcom>
2020-10-07 16:42:08 +0000

    commit 8

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
+import location from './location'
 
 function App() {
-
-  const dateBuilder = (d) => {
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-      
-      return `${day} ${date} ${month} ${year}`
-   }
-
   return (
     <div className="app">
       <main>
@@ -26,10 +14,7 @@ function App() {
             placeholder="Search......"
           />
         </div>
-        <div className="location-box">
-          <div className="location">Accra, Ghana</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
+         <location/> 
         <div className="weather-box">
           <div className="temp">27&deg; </div>
           <div className="icon">
---
+++ src/location.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function location(){
+
+ const dateBuilder = (d) => {
+   let months = [
+     "January",
+     "February",
+     "March",
+     "April",
+     "May",
+     "June",
+     "July",
+     "August",
+     "September",
+     "October",
+     "November",
+     "December",
+   ];
+   let days = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday",
+   ];
+
+   let day = days[d.getDay()];
+   let date = d.getDate();
+   let month = months[d.getMonth()];
+   let year = d.getFullYear();
+
+   return `${day} ${date} ${month} ${year}`;
+ };
+
+    return (
+      <div className="location-box">
+        <div className="location">Accra, Ghana</div>
+        <div className="date">{dateBuilder(new Date())}</div>
+      </div>
+    );
+}
+
+export default location;
