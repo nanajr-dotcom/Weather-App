103a0aecae4b7ff1a6bfd62ddd28ddf6e0a9bb0f
asamoah@goldmail.etsu.edu <nanajr-dotcom>
2020-10-07 19:34:07 +0000

    commit 11

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
-import Location from './Location';
-import Weatherbox from './Weatherbox';
-import Searchbox from './Searchbox';
+import Main from './Main';
 
 
 function App() {
   return (
     <div className="app">
       <main>
-         <Searchbox/>
-         <Location/> 
-         <Weatherbox/> 
+         <Main/> 
       </main>
     </div>
   );
---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Searchbox from './Searchbox';
+import Location from './Location';
+import Weatherbox from './Weatherbox';
+
+ function Main(){
+    return(
+        <>
+        <Searchbox/>
+         <Location/> 
+         <Weatherbox/> 
+         </>
+
+    )
+
+}
+
+
+export default Main;
